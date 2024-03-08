2f5b093f2a930f51e03f23d8257fee06dbefc147
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2024-03-08 11:01:03 +0100

    Initial commit

---
+++ src/main/java/com/gabodev/bankclientbackend/BankClientBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gabodev.bankclientbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankClientBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankClientBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabodev/bankclientbackend/BankClientBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabodev.bankclientbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankClientBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
